815fe494c6fcc25ca92757e502f4d707e41fb927
Gabriel Felipe <gabri26.cost@gmail.com>
2025-11-06 10:54:40 -0300

    Livros retornando Json

--- Models/Livros.cs
+++ Models/Livros.cs
@@ -1,22 +1,17 @@
-using System;
-using System.Collections;
-
 namespace Biblioteca.Models
 {
     public class Livros
     {
-        public int Id;
-        public string Titulo;
-        public string Autor;
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
 
         public Livros(string titulo, string autor)
         {
             Titulo = titulo;
             Autor = autor;
         }
-
-        List<Livros> livraria = new List<Livros>();
+        
+        public Livros() { }
     }
-
-
 }
--- Program.cs
+++ Program.cs
@@ -5,15 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 List<Livros> livros = new List<Livros>();
-livros.Add(new Livros("Das Kapital", "Karl Marx"));
-livros.Add(new Livros("Left and Right", "Noberto Bobbio"));
-livros.Add(new Livros("State, Society and Goverment", "Noberto Bobbio"));
-livros.Add(new Livros("1984", "George Orwell"));
-app.MapGet("/", () =>
+
+void AdicionarLivro(string titulo, string autor)
 {
-    foreach (var livro in livros)
-    {
-        Console.WriteLine($"Titulo: {livro.Titulo}| Autor: {livro.Autor}");
-    }
-});
+    var novoId = livros.Any() ? livros.Max(l => l.Id) + 1 : 1;
+    livros.Add(new Livros(titulo, autor) { Id = novoId });
+}
+
+AdicionarLivro("Das Kapital", "Karl Marx");
+AdicionarLivro("Left and Right", "Noberto Bobbio");
+AdicionarLivro("State, Society and Government"  , "Noberto Bobbio");
+AdicionarLivro("1984", "George Orwell");
+
+app.MapGet("/", () => livros);
 app.Run();
